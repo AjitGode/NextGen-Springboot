356c391f76215524e1e64f58f39d4628c91f26d6
AjitGode <godeajit71@gmail.com>
2025-02-13 13:09:36 +0530

    Change the Time Zone

--- src/main/java/com/NextGen/NextGenInstitute/NextGenInstituteApplication.java
+++ src/main/java/com/NextGen/NextGenInstitute/NextGenInstituteApplication.java
@@ -3,10 +3,13 @@ package com.NextGen.NextGenInstitute;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class NextGenInstituteApplication {
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Kolkata"));
 		SpringApplication.run(NextGenInstituteApplication.class, args);
 	}
 
